3af1c2ccf71aab52dc50c956a9adda7c06954a8c
flyqk <flyqk1991@gmail.com>
2015-03-27 09:59:35 -0700

    sample app added

---
+++ example/Cordova Geotrigger/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/Carl/Desktop/vresorts/cordova-plugin-background-geolocation-master/example/SampleApp/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/Carl/Desktop/vresorts/cordova-plugin-background-geolocation-master/example/SampleApp/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ example/Cordova Geotrigger/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,33 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.transistorsoft.cordova.background-geolocation/www/BackgroundGeoLocation.js",
+        "id": "org.transistorsoft.cordova.background-geolocation.BackgroundGeoLocation",
+        "clobbers": [
+            "plugins.backgroundGeoLocation"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/android/notification.js",
+        "id": "org.apache.cordova.dialogs.notification_android",
+        "merges": [
+            "navigator.notification"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.transistorsoft.cordova.background-geolocation": "0.3.6",
+    "org.apache.cordova.geolocation": "0.3.12",
+    "org.apache.cordova.dialogs": "0.3.0"
+}
+// BOTTOM OF METADATA
+});
